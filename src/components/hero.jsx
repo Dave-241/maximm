e0089e121e4b28049ee8c5ad03bda0db3013f69e
DAVIS ISIBOR <davisisibor@gmail.com>
2024-03-22 10:26:43 +0100

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -13,14 +13,29 @@ const Hero_section = () => {
     <>
       <div className=" w-full flex justify-between flex-col md:flex-row md:gap-0 gap-[5rem]  md:px-[8%] mb-[5rem] px-[3%] ">
         <div className="flex px-[4%] w-full flex-col md:gap-[2rem] gap-[1.2rem]">
-          <h1 className="text-white md:text-[4rem] text-[2.7rem] leading-[3.5rem] md:leading-[5rem] modal-title ">
+          <h1
+            className={`text-white md:text-[4rem] text-[2.7rem] leading-[3.5rem] md:leading-[5rem] modal-title  ${
+              start_animation ? " translate-y-0" : " translate-y-[5rem]"
+            } `}
+            style={{ transition: "1s ease" }}
+          >
             Be a part of the <br /> future of finance
           </h1>
-          <p className="text-white modal-title  text-opacity-[40%] md:text-[1.2rem] text-[1rem]">
+          <p
+            className={`text-white modal-title  text-opacity-[40%] md:text-[1.2rem] text-[1rem]  ${
+              start_animation ? " translate-y-0" : " translate-y-[5rem]"
+            } `}
+            style={{ transition: "1s ease" }}
+          >
             Take your portfolio to the next level, invest <br /> in a secure and
             innovative technology
           </p>
-          <div className="flex md:gap-[2.5rem] pr-[2%] modal-title  md:justify-start w-full justify-between">
+          <div
+            className={`flex md:gap-[2.5rem]  ${
+              start_animation ? " translate-y-0" : " translate-y-[5rem]"
+            } pr-[2%] modal-title  md:justify-start w-full justify-between`}
+            style={{ transition: "1s ease" }}
+          >
             <button
               className={` bg-[#272DE6]  transition duration-[0.6s] flex justify-center hover:bg-black md:gap-[0.6rem] items-center md:rounded-[2rem] gap-[0.5rem] md:text-[1.2rem] px-[1rem] py-[0.5rem] rounded-[2rem] md:px-0 md:py-[0.7rem] border border-[#272DE6] hover:border-white text-white`}
             >
